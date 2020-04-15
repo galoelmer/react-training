bc6449435f944fe1ccac60c42ab819b1a9bff385
Elmer C. Galo <elmer.galo@outlook.com>
2020-04-15 15:51:12 -0400

    Accessing Context: Created a context folder so other components can have access to its data

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
-    render() {
-        return (
-            <div className="book-list">
-                <ul>
-                    <li>Book 1</li>
-                    <li>Book 2</li>
-                    <li>Book 3</li>
-                </ul>
-            </div>
-        );
-    }
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>Book 2</li>
+          <li style={{ background: theme.ui }}>Book 3</li>
+          <li style={{ background: theme.ui }}>Book 1</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default BookList;
