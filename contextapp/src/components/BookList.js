1737718f4e2254b0698f4b07a3171775134340e0
Elmer C. Galo <elmer.galo@outlook.com>
2020-04-16 21:51:00 -0400

    Change the Booklist class componet to functional component and use the useContext hook

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,24 +1,43 @@
-import React, { Component } from "react";
+// eslint-disable-next-line
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>Book 2</li>
-          <li style={{ background: theme.ui }}>Book 3</li>
-          <li style={{ background: theme.ui }}>Book 1</li>
-        </ul>
-      </div>
-    );
-  }
-}
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return (
+//       <div
+//         className="book-list"
+//         style={{ color: theme.syntax, background: theme.bg }}
+//       >
+//         <ul>
+//           <li style={{ background: theme.ui }}>Book 2</li>
+//           <li style={{ background: theme.ui }}>Book 3</li>
+//           <li style={{ background: theme.ui }}>Book 1</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// Implementing functional componets with useContext hook
+const BookList = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Book 2</li>
+        <li style={{ background: theme.ui }}>Book 3</li>
+        <li style={{ background: theme.ui }}>Book 1</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
