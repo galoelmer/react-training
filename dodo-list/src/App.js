5558b2c49917989362f75f3eb4682d1432092ec4
Elmer C. Galo <elmer.galo@outlook.com>
2020-04-22 22:33:57 -0400

    TODO app initial setup

---
+++ dodo-list/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App ui raised very padded text container segment">
+      <div className="ui blue center aligned huge header">Todo's List</div>
+    </div>
+  );
+}
+
+export default App;
