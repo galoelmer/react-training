711ae575183238a09bf0f2c29485b8e8e9b45d22
Elmer C. Galo <elmer.galo@outlook.com>
2020-05-01 14:01:41 -0400

    Initial app setup

---
+++ planning-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h2 className="ui red block header">Planning App</h2>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
