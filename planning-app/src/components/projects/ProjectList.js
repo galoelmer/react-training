f127300f42cecbea2243ff1796a5a4a6594c492a
Elmer C. Galo <elmer.galo@outlook.com>
2020-05-03 13:47:18 -0400

    Add project summary component

--- planning-app/src/components/projects/ProjectList.js
+++ planning-app/src/components/projects/ProjectList.js
@@ -1,53 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list ui list container">
-      <div className="item">
-        <div className="ui card">
-          <div className="content">
-            <div className="header">Cute Dog</div>
-            <div className="meta">2 days ago</div>
-            <div className="description">
-              <p>
-                Cute dogs come in a variety of shapes and sizes. Some cute dogs
-                are cute for their adorable faces, others for their tiny
-                stature, and even others for their massive size.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="item">
-        <div className="ui card">
-          <div className="content">
-            <div className="header">Cute Dog</div>
-            <div className="meta">2 days ago</div>
-            <div className="description">
-              <p>
-                Cute dogs come in a variety of shapes and sizes. Some cute dogs
-                are cute for their adorable faces, others for their tiny
-                stature, and even others for their massive size.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="item">
-        <div className="ui card">
-          <div className="content">
-            <div className="header">Cute Dog</div>
-            <div className="meta">2 days ago</div>
-            <div className="description">
-              <p>
-                Cute dogs come in a variety of shapes and sizes. Some cute dogs
-                are cute for their adorable faces, others for their tiny
-                stature, and even others for their massive size.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
