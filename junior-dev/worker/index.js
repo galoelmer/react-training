fe2d4a9b6b280df1d99d938e4738d973a1ecfe1f
Elmer C. Galo <elmer.galo@outlook.com>
2020-05-09 13:28:11 -0400

    Add redis config

--- junior-dev/worker/index.js
+++ junior-dev/worker/index.js
@@ -3,12 +3,4 @@ var CronJob = require('cron').CronJob;
 const fetchGitHub = require('./tasks/fetch-github');
 
 // fetch github jobs
-var job = new CronJob(
-  '* * * * *',
-  function () {
-    fetchGitHub;
-  },
-  null,
-  true,
-  'America/Los_Angeles'
-);
+new CronJob('* * * * *', fetchGitHub, null, true, 'America/Los_Angeles');
--- junior-dev/worker/tasks/fetch-github.js
+++ junior-dev/worker/tasks/fetch-github.js
@@ -1,4 +1,10 @@
 var fetch = require('node-fetch');
+var redis = require('redis'),
+  client = redis.createClient();
+
+const { promisify } = require('util');
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 const baseUrl = 'https://jobs.github.com/positions.json';
 
@@ -8,19 +14,23 @@ async function fetchGitHub() {
   const allJobs = [];
 
   while (resultCount > 0) {
-    const res = await fetch(`${baseUrl}?page=${onPage}`);
-    const jobs = await res.json();
-    allJobs.push(...jobs);
-    resultCount = jobs.length;
-    console.log('Got', resultCount, 'jobs');
-    onPage++;
+    try {
+      const res = await fetch(`${baseUrl}?page=${onPage}`);
+      const jobs = await res.json();
+      allJobs.push(...jobs);
+      resultCount = jobs.length;
+      console.log('Got', resultCount, 'jobs');
+      onPage++;
+    } catch (err) {
+      console.log(err);
+    }
   }
-
   console.log('got', allJobs.length, 'jobs');
+  
+  const success = await setAsync('github', JSON.stringify(allJobs));
+  console.log({ success });
 }
 
-fetchGitHub().catch(function (err) {
-  console.log('Error', err);
-});
+fetchGitHub();
 
 module.exports = fetchGitHub;
