e576dd211ce20b1db0941c68564be41f273cc65b
Elmer C. Galo <elmer.galo@outlook.com>
2020-05-09 14:15:58 -0400

    Show fetch data on front end

--- junior-dev/client/src/App.js
+++ junior-dev/client/src/App.js
@@ -3,16 +3,25 @@ import { Container } from 'semantic-ui-react';
 
 import Jobs from './Jobs';
 
-const mockJobs = [
-  {title: 'SWE 1', company: "Amazon"},
-  {title: 'SWE 1', company: "Facebook"},
-  {title: 'SWE 1', company: "Google"}
-]
+const JOB_API_URL = 'http://localhost:3001/jobs';
+
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  updateCb(json);
+}
 
 function App() {
+
+  const [jobsList, updateJobs] = React.useState([]);
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  }, [])
+
   return (
     <Container textAlign="center">
-      <Jobs jobs={mockJobs}/>
+      <Jobs jobs={jobsList}/>
     </Container>
   );
 }
