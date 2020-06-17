a6aea0fe7bba768ef4f94ada9c036bc71240cd64
Elmer C. Galo <elmer.galo@outlook.com>
2020-06-17 17:38:11 -0400

    Add infinite scroll with a custom hook with a debounce funcionality for scrolling

--- hacker-news/src/containers/StoriesContainer.js
+++ hacker-news/src/containers/StoriesContainer.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import { getStoryIds } from '../services/hnAPi';
 import { Story } from '../components/Story';
+import { useInfiniteScroll } from '../hooks/useInfiniteScroll';
 
 export const StoriesContainer = () => {
+  const { count } = useInfiniteScroll();
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryIds().then((data) => setStoryIds(data));
-  }, []);
-  
-  return storyIds.map(storyId => <Story key={storyId} storyId={storyId} />);
+    console.log(count);
+  }, [count]);
+
+  return storyIds
+    .slice(0, count)
+    .map((storyId) => <Story key={storyId} storyId={storyId} />);
 };
