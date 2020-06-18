26e846457c473136db8279f95e73654e6478eda2
Elmer C. Galo <elmer.galo@outlook.com>
2020-06-18 17:57:40 -0400

    Initial file setup to create a beach resort app

---
+++ beach-resort-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, CssBaseline, Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Box display="flex" justifyContent="center">
+        <Typography variant="h3">Beach Resort App</Typography>
+      </Box>
+    </>
+  );
+}
+
+export default App;
