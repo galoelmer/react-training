d0ced4bb559e5a5f7dd27a5bc315485591ae1a44
Elmer C. Galo <elmer.galo@outlook.com>
2020-07-02 18:19:19 -0400

    RoomsList component now shows a list of sorted card(rooms)

--- beach-resort-app/src/components/RoomsList.js
+++ beach-resort-app/src/components/RoomsList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Room from '../components/Room';
+import { Container, Grid } from '@material-ui/core';
 
-export default function RoomsList() {
+export default function RoomsList({ rooms }) {
+  if (rooms.length === 0) {
     return (
-        <div>
-            Hello from rooms list
-        </div>
-    )
+      <div>
+        <h5>No rooms matched your search paramenters</h5>
+      </div>
+    );
+  }
+  return (
+    <Container>
+      <Grid container spacing={0}>
+        {rooms.map((item) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={item.id}>
+              <Room room={item} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
 }
