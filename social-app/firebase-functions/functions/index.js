2e8cd9feaa8ebe9a37c4350320851a875e9a9fda
Elmer C. Galo <elmer.galo@outlook.com>
2020-07-14 23:24:07 -0400

    Add input validation to signup users

--- social-app/firebase-functions/functions/index.js
+++ social-app/firebase-functions/functions/index.js
@@ -62,6 +62,17 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+};
+
+const isEmail = (email) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regex)) return true;
+  else return false;
+};
+
 // Signup route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -71,8 +82,23 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle,
   };
 
-  // TODO: validate data
+  let errors = {};
 
+  // Validate user input data
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = 'Passwords must match';
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then((doc) => {
@@ -85,14 +111,29 @@ app.post('/signup', (req, res) => {
       }
     })
     .then((data) => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then((token) => {
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then((data) => {
       return res.status(201).json({ token });
     })
     .catch((err) => {
       console.log(err);
-      return res.status(500).json({ error: err.code });
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already in use' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
     });
 });
 
