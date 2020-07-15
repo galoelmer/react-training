339cea5a2b6b4b0d0ce667c053400b0a4aaeb1f5
Elmer C. Galo <elmer.galo@outlook.com>
2020-07-14 22:11:11 -0400

    Add Signup route

--- social-app/firebase-functions/functions/index.js
+++ social-app/firebase-functions/functions/index.js
@@ -6,6 +6,19 @@ admin.initializeApp();
 const express = require('express');
 const app = express();
 
+var config = {
+  apiKey: 'AIzaSyD-e3RkSvgqWl9rf5v8KZEDWelqY5UO9SE',
+  authDomain: 'social-app-d1d64.firebaseapp.com',
+  databaseURL: 'https://social-app-d1d64.firebaseio.com',
+  projectId: 'social-app-d1d64',
+  storageBucket: 'social-app-d1d64.appspot.com',
+  messagingSenderId: '329622600728',
+  appId: '1:329622600728:web:3a8aa715726d35fbfdd76d',
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
 app.get('/screams', async (req, res) => {
   const result = await admin
     .firestore()
@@ -47,6 +60,30 @@ app.post('/scream', (req, res) => {
     .catch((err) => {
       res.status(500).json({ error: 'Something went wrong' });
       console.log(err);
+    });
+});
+
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // TODO: validate data
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    }).catch((err) => {
+      console.log(err);
+      return res.status(500).json({error: err.code})
     });
 });
 
