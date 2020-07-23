566f91e3d7dcabe528c0c1b986844d3a81527973
Elmer C. Galo <elmer.galo@outlook.com>
2020-07-23 00:09:10 -0400

    Add firebase functions to notifify when user like and unlike a post

--- social-app/firebase-functions/functions/index.js
+++ social-app/firebase-functions/functions/index.js
@@ -17,6 +17,7 @@ const {
   addUserDetails,
   getUserDetails,
 } = require('./handlers/users');
+const { db } = require('./util/admin');
 
 // Scream Routes
 app.get('/screams', getAllScreams);
@@ -35,3 +36,38 @@ app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getUserDetails);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document('likes/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+
+exports.deleteNotificationOnUnlike = functions.firestore
+  .document('likes/{id}')
+  .onDelete((snapshot) => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch((err) => {
+        console.log(err);
+      });
+  });
