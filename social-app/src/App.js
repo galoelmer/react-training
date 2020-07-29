ce2b50de334ec4a9a5104e70c2f983640379be4a
Elmer C. Galo <elmer.galo@outlook.com>
2020-07-28 20:44:27 -0400

    Add Material-UI theme provider

--- social-app/src/App.js
+++ social-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 // Material UI Components
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 //Components
 import Navbar from './components/Navbar';
@@ -13,20 +14,39 @@ import Home from './pages/home';
 import Login from './pages/login';
 import SignUp from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <CssBaseline />
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-          </Switch>
-        </div>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={SignUp} />
+            </Switch>
+          </div>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
