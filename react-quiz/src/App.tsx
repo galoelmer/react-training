0d7558199ae1e547ad804616872fac8242790189
Elmer C. Galo <elmer.galo@outlook.com>
2020-09-03 23:31:36 -0400

    Add game logic

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchQuizQuestions } from './API';
 
-function App() {
+// components
+import QuestionCard from './components/QuestionCard';
+
+// Types
+import { Difficulty, QuestionState } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      if (correct) setScore((prev) => prev + 1);
+      const AnswerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, AnswerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
   return (
     <div>
-      Quiz
+      <h1>Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p>Score: {score} </p> : null}
+      {loading && <p>Loading question...</p>}
+      {!loading && !gameOver && (
+        <>
+          <QuestionCard
+            questionNum={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        </>
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button onClick={nextQuestion}>Next question</button>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
---
+++ react-quiz/src/components/QuestionCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: any;
+    questionNum: number;
+    totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNum,
+    totalQuestions,
+}) => (
+        <div>
+            <p>
+                Question: {questionNum} / {totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{ __html: question }} />
+            <div>
+                {answers.map((answer) => (
+                    <div key={answer}>
+                        <button disabled={userAnswer} value={answer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
+export default QuestionCard;
