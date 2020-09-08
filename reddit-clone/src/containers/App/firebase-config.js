d604e3d91d657dbaefd0c1bb84a17b6fa2d9ece2
Elmer C. Galo <elmer.galo@outlook.com>
2020-09-08 17:42:13 -0400

    Add firebase configuration

---
+++ reddit-clone/src/containers/App/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyBF6ZyA79HBknLFGg2iVidKCFLqiOtUF1I',
+  authDomain: 'reddit-clone-a9de0.firebaseapp.com',
+  databaseURL: 'https://reddit-clone-a9de0.firebaseio.com',
+  projectId: 'reddit-clone-a9de0',
+  storageBucket: 'reddit-clone-a9de0.appspot.com',
+  messagingSenderId: '375411822522',
+  appId: '1:375411822522:web:bbe31c112dab6ec1c69783',
+  measurementId: 'G-QBZJZDWPJQ',
+};
--- reddit-clone/src/containers/App/index.js
+++ reddit-clone/src/containers/App/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as firebase from 'firebase/app';
+import config from './firebase-config';
+import 'firebase/database';
+
+firebase.initializeApp(config);
+const db = firebase.database();
 
 const App = () => {
+  const [posts, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let postRef = db.ref('posts');
+    postRef.on('value', (snapshot) => {
+      console.log(snapshot.val());
+      setPost(snapshot.val());
+      setLoading(false);
+    });
+  }, []);
   return <div>Hello World</div>;
 };
 
